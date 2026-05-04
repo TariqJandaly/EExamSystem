2a1f5b80eefcbfedb2f12e83074e243309a11682
Yasser Al-Awfi <Foxtoy00@gmail.com>
2026-05-04 23:00:11 +0300

    added frontend

--- EExamSystem.Client/Program.cs
+++ EExamSystem.Client/Program.cs
@@ -1,4 +1,5 @@
 using EExamSystem.Client.Components;
+using EExamSystem.Client.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddHttpClient();
+builder.Services.AddScoped<LanguageService>();
 
 var app = builder.Build();
 
