fe38f90b8decfc34f73be8e32dd5698a87844286
Yasser Alghamdi <ya3sa6@gmail.com>
2026-05-06 23:55:36 +0300

    feat(api): Implement IUserService interface for user management operations

---
+++ EExamSystem.Api/Interfaces/IUserService.cs
@@ -0,0 +1,21 @@
+using EExamSystem.Shared.DTOs.Users;
+using EExamSystem.Shared.DTOs;
+namespace EExamSystem.Api.Interfaces;
+
+
+/// <summary>
+/// Interface for managing users.
+/// </summary>
+public interface IUserService
+{
+    Task<ServiceResponse<List<UserDto>>> GetAllUsersAsync();
+    Task<ServiceResponse<UserDto>> GetUserAsync(string id);
+    Task<ServiceResponse<UserDto>> CreateUserAsync(UserCreateDto userCreateDto);
+    Task<ServiceResponse<UserDto>> DeleteUserAsync(string id);
+    Task<ServiceResponse<UserDto>> UpdateUserAsync(string id, UserCreateDto userCreateDto);
+    Task<ServiceResponse<UserRolesDto>> GetUserRolesAsync(string id);
+    Task<ServiceResponse<UserRolesDto>> AddUserRolesAsync(string id, UserRolesDto userRolesDto);
+    Task<ServiceResponse<UserRolesDto>> RemoveUserRolesAsync(string id, UserRolesDto userRolesDto);
+    Task<ServiceResponse> ChangeUserPasswordAsync(string id, UserChangePasswordDto changePasswordDto);
+
+}
